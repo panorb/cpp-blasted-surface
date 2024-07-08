e9bc6d7750e97ff1185e4690cc4d3a6376bf862c
Paul Norberger <hi@paulnorberger.de>
2024-07-08 14:58:17 +0200

    Tiny namespace syntax sugar fix

--- source/core/include/blast/log.hpp
+++ source/core/include/blast/log.hpp
@@ -2,41 +2,39 @@
 #include <spdlog/spdlog.h>
 #include <spdlog/cfg/env.h>
 
-namespace blast {
-	namespace log {
-		inline void init()
-		{
-			spdlog::cfg::load_env_levels();
-		}
+namespace blast::log {
+	inline void init()
+	{
+		spdlog::cfg::load_env_levels();
+	}
 
-		template<typename T>
-		void info(const T& msg)
-		{
-			spdlog::info(msg);
-		}
+	template<typename T>
+	void info(const T& msg)
+	{
+		spdlog::info(msg);
+	}
 
-		template<typename T>
-		void warn(const T& msg) {
-			spdlog::warn(msg);
-		}
+	template<typename T>
+	void warn(const T& msg) {
+		spdlog::warn(msg);
+	}
 
-		template<typename T>
-		void error(const T& msg)
-		{
-			spdlog::error(msg);
-		}
+	template<typename T>
+	void error(const T& msg)
+	{
+		spdlog::error(msg);
+	}
 
-		template<typename T>
-		void debug(const T& msg)
-		{
-			spdlog::debug(msg);
-		}
+	template<typename T>
+	void debug(const T& msg)
+	{
+		spdlog::debug(msg);
+	}
 
-		template<typename T>
-		void trace(const T& msg)
-		{
-			spdlog::trace(msg);
-		}
+	template<typename T>
+	void trace(const T& msg)
+	{
+		spdlog::trace(msg);
 	}
 }
 
